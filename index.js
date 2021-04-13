d62e2146c33ad01f8204e77b648135364aca4827
jon <jonprosser84@gmail.com>
2021-04-13 08:19:47 +0100

    completed more jonas-node

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const fs = require('fs');
+const http = require('http');
+
+
+//////////////////////////////////////////////////////////////////
+//files
 
 //blockings, sychronus way
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('file written');
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('file written');
 
-// non blocking, asychronos way
-fs.readFile('./txt/start.txt');
+// // non blocking, asychronos way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             console.log(data3);
+
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//                 console.log('file has been written');
+//             });
+//         });
+//     });
+// });
+
+// console.log('will read file');
+//////////////////////////////////////////////////////////////////
+//Server
+
+http.createServer((req, res) => {
+    res.end('Hello from the server!');
+});
